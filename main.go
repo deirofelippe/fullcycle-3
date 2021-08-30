4855c787e4fb98f9fb3aef07521bb65d6a4f99d6
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-08-29 21:24:28 -0300

    funcao main para testar o codigo

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/felippedesouza/fullcycle3-codebank/domain"
+	"github.com/felippedesouza/fullcycle3-codebank/infrastructure/repository"
+	"github.com/felippedesouza/fullcycle3-codebank/usecase"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db := setupDb()
+	defer db.Close()
+
+	cc := domain.NewCreditCard()
+	cc.Number = "1234"
+	cc.Name = "Felippe"
+	cc.ExpirationYear = 2021
+	cc.ExpirationMonth = 9
+	cc.CVV = 123
+	cc.Limit = 1000
+	cc.Balance = 0
+
+	repo := repository.NewTransactionRepositoryDb(db)
+	err := repo.CreateCreditCard(*cc)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+	transactionRepository := repository.NewTransactionRepositoryDb(db)
+	usecase := usecase.NewUseCaseTransaction(transactionRepository)
+	return usecase
+}
+
+func setupDb() *sql.DB {
+	psqlinfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"db",
+		"5432",
+		"postgres",
+		"root",
+		"codebank",
+	)
+	db, err := sql.Open("postgres", psqlinfo)
+	if err != nil {
+		log.Fatal("error connection to database")
+	}
+	return db
+}
