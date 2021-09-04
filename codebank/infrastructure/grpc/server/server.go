89cdeb957b57c5c72c0293869acb4fcc041b0899
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-04 16:42:21 -0300

    configurando servidor gRPC

---
+++ codebank/infrastructure/grpc/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	"github.com/felippedesouza/fullcycle3-codebank/infrastructure/grpc/pb"
+	"github.com/felippedesouza/fullcycle3-codebank/infrastructure/grpc/service"
+	"github.com/felippedesouza/fullcycle3-codebank/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (g GRPCServer) Serve() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50052")
+	if err != nil {
+		log.Fatalf("could not listen tcp port")
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(lis)
+}
