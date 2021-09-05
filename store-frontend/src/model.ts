b7cb425b706dca28e1756d1afab4bb516062c9ff
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-05 17:33:13 -0300

    usando axios e criando modelo de produto

---
+++ store-frontend/src/model.ts
@@ -0,0 +1,30 @@
+export interface Product{
+   id: string;
+   name: string;
+   description: string;
+   image_url: string;
+   slug: string;
+   price: number;
+   created_at: string;
+}
+
+export const products: Product[] = [
+   {
+      id: 'uuid-1',
+      name: 'produto',
+      description: 'muito',
+      price: 50.50,
+      image_url: 'https://source.unsplash.com/random?product',
+      slug: 'produto-teste',
+      created_at: '2021-06-06T00:00:00'
+   },
+   {
+      id: 'uuid-2',
+      name: 'produto2',
+      description: 'mt',
+      price: 500.50,
+      image_url: 'https://source.unsplash.com/random?product',
+      slug: 'produto-teste-1',
+      created_at: '2021-06-06T00:00:00'
+   }
+]
