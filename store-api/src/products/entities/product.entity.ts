c254a9e6c6a1e5541d6b63e9665b1e37214ac494
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-06 17:24:43 -0300

    mapeando a classe product pro orm

--- store-api/src/products/entities/product.entity.ts
+++ store-api/src/products/entities/product.entity.ts
@@ -1,1 +1,22 @@
-export class Product {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'products' })
+export class Product {
+   @PrimaryGeneratedColumn('uuid')
+   id: string;
+
+   @Column()
+   name: string;
+
+   @Column()
+   description: string;
+
+   @Column()
+   image_url: string;
+
+   @Column()
+   slug: string;
+
+   @CreateDateColumn({ type: 'timestamp' })
+   created_at: string;
+}
