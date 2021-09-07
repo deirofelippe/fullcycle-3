8d26257ddc1c923178859b674b46a48e2258fd64
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 18:50:38 -0300

    cria campos da tabela dos migrations de orders e order-items

--- store-api/src/migrations/1631023347114-create-order-items-table.ts
+++ store-api/src/migrations/1631023347114-create-order-items-table.ts
@@ -1,11 +1,68 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
-export class createOrderItemsTable1631023347114 implements MigrationInterface {
+export class CreateOrderItemsTable1631023347114 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+   public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+         name: 'order_items',
+         columns: [
+            {
+               name: 'id',
+               type: 'uuid',
+               isPrimary: true
+            },
+            {
+               name: 'quantity',
+               type: 'int',
+            },
+            {
+               name: 'price',
+               type: 'double precision',
+            },
+            {
+               name: 'product_id',
+               type: 'uuid',
+            },
+            {
+               name: 'order_id',
+               type: 'uuid',
+            },
+         ]
+      }))
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+      await queryRunner.createForeignKey(
+         'order_items',
+         new TableForeignKey({
+            name: 'order_items_product_id_foreign_key',
+            columnNames: ['product_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'products'
+         })
+      )
+
+      await queryRunner.createForeignKey(
+         'order_items',
+         new TableForeignKey({
+            name: 'order_items_order_id_foreign_key',
+            columnNames: ['order_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'orders'
+         })
+      )
+   }
+
+   public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey(
+         'order_items',
+         'order_items_product_id_foreign_key',
+      )
+
+      await queryRunner.dropForeignKey(
+         'order_items',
+         'order_items_order_id_foreign_key',
+      )
+
+      await queryRunner.dropTable('order_items')
+   }
 
 }
