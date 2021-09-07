bfd464091e41d232ad283780533067783e48aa6a
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 18:54:13 -0300

    importando as entidades pro modulo de orders

--- store-api/src/orders/orders.module.ts
+++ store-api/src/orders/orders.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
-  controllers: [OrdersController],
-  providers: [OrdersService]
+   imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
+   controllers: [OrdersController],
+   providers: [OrdersService]
 })
-export class OrdersModule {}
+export class OrdersModule { }
