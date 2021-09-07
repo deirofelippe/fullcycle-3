058d528e5367729117406d60fde78198a318e75f
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 18:47:59 -0300

    crinado validacao customizavel

---
+++ store-api/src/validators/exists.rule.ts
@@ -0,0 +1,46 @@
+import { registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+import { EntityNotFoundError, getConnection } from "typeorm";
+
+export function Exists(
+   entityClass: any,
+   field = 'id',
+   validationOptions?: ValidationOptions
+) {
+   return function (object: any, propertyName: string) {
+      registerDecorator({
+         name: 'Exists',
+         constraints: [entityClass, field],
+         target: object.constructor,
+         propertyName: propertyName,
+         options: validationOptions,
+         validator: ExistsRule
+      })
+   }
+}
+
+@ValidatorConstraint({ name: 'Exists', async: true })
+export class ExistsRule implements ValidatorConstraintInterface {
+   async validate(value: string, args: ValidationArguments) {
+      if (!value) return false;
+      try {
+         const [entityClass, field] = args.constraints
+         const conn = getConnection('default')
+         const repository = conn.getRepository(entityClass)
+         const result = await repository.findOne({
+            where: {
+               [field]: value
+            }
+         })
+         if (!result) throw new EntityNotFoundError(entityClass, value)
+         return true
+      } catch (error) {
+         console.error(error);
+         return false
+      }
+   }
+   
+   defaultMessage(args: ValidationArguments) {
+      const [modelClass] = args.constraints
+      return `${modelClass.name} not found`
+   }
+}
