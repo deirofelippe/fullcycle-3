2ac6bd38e410a4c9aaa8959b2c86870bbd8593a0
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 10:44:06 -0300

    crinado erro 404 customizado

--- store-api/src/main.ts
+++ store-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFouncExceptionFilter } from './exception-filters/entity-not-found-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new EntityNotFouncExceptionFilter())
   await app.listen(3000);
 }
 bootstrap();
