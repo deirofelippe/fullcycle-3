13724b84878ba7e05722a1d1a4b024649c938412
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 10:47:06 -0300

    importando modulo pro nest conectar com typeorm

--- store-api/src/app.module.ts
+++ store-api/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [
+      ConfigModule.forRoot(),
+      ProductsModule,
+      TypeOrmModule.forRoot({
+         type: process.env.TYPEORM_CONNECTION as any,
+         host: process.env.TYPEORM_HOST,
+         port: parseInt(process.env.TYPEORM_PORT),
+         username: process.env.TYPEORM_USERNAME,
+         password: process.env.TYPEORM_PASSWORD,
+         database: process.env.TYPEORM_DATABASE,
+         entities: [process.env.TYPEORM_ENTITIES],
+      })
+   ],
+   controllers: [AppController],
+   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
