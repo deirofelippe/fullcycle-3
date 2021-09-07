71531206a60aa41a66e2e4f9e9182aab7692c812
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 10:57:05 -0300

    validacao de products

--- store-api/src/main.ts
+++ store-api/src/main.ts
@@ -1,10 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFouncExceptionFilter } from './exception-filters/entity-not-found-exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new EntityNotFouncExceptionFilter())
-  await app.listen(3000);
+   const app = await NestFactory.create(AppModule);
+   app.useGlobalFilters(new EntityNotFouncExceptionFilter())
+   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }))
+   await app.listen(3000);
 }
 bootstrap();
