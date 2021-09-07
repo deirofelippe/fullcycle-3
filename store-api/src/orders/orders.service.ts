aaf44ce6ff3b1c19cf55d028728f71170c27551c
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-07 18:52:57 -0300

    operacao create pra orders

--- store-api/src/orders/orders.service.ts
+++ store-api/src/orders/orders.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { In, Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
-  }
+   constructor(
+      @InjectRepository(Order) private orderRepo: Repository<Order>,
+      @InjectRepository(Product) private productRepo: Repository<Product>
+   ) { }
 
-  findAll() {
-    return `This action returns all orders`;
-  }
+   async create(createOrderDto: CreateOrderDto) {
+      const order = this.orderRepo.create(createOrderDto)
+      const products = await this.productRepo.find({
+         where: {
+            id: In(order.items.map(item => item.product_id))
+         }
+      })
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
+      order.items.forEach(item => {
+         const product = products.find(
+            product => product.id === item.product_id
+         )
+         item.price = product.price
+      })
+      
+      return this.orderRepo.save(order)
+   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
+   findAll() {
+      return `This action returns all orders`;
+   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
+   findOne(id: number) {
+      return `This action returns a #${id} order`;
+   }
+
+   update(id: number, updateOrderDto: UpdateOrderDto) {
+      return `This action updates a #${id} order`;
+   }
+
+   remove(id: number) {
+      return `This action removes a #${id} order`;
+   }
 }
