8a37fe7deb71782f5a3b861aa308620855ec2779
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-09 11:09:14 -0300

    trocando porta no docker compose, adicionando pacotes e .env

--- store-api/src/main.ts
+++ store-api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { EntityNotFouncExceptionFilter } from './exception-filters/entity-not-found-exception-filter';
 
 async function bootstrap() {
-   const app = await NestFactory.create(AppModule);
+   const app = await NestFactory.create(AppModule, { cors: true });
    app.useGlobalFilters(new EntityNotFouncExceptionFilter())
    app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }))
    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
