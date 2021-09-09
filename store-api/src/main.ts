f9927d90a6e28a7a331de6b51db294469d41eaab
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-09 12:34:56 -0300

    RpcException, erro customizado

--- store-api/src/main.ts
+++ store-api/src/main.ts
@@ -1,11 +1,15 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { EntityNotFouncExceptionFilter } from './exception-filters/entity-not-found-exception-filter';
+import { EntityNotFouncExceptionFilter } from './exception-filters/entity-not-found.exception-filter';
+import { RpcExceptionFilter } from './exception-filters/rpc.exception-filter';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule, { cors: true });
-   app.useGlobalFilters(new EntityNotFouncExceptionFilter())
+   app.useGlobalFilters(
+      new EntityNotFouncExceptionFilter(),
+      new RpcExceptionFilter()
+   )
    app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }))
    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
    await app.listen(3000);
