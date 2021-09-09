e49f7ea4e593f69411e1cd55ba6829c64bef652e
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-09 11:05:07 -0300

    service pro payment

--- store-api/src/orders/payment/payment.service.ts
+++ store-api/src/orders/payment/payment.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+interface PaymentData {
+   creditCard: {
+      number: string;
+      name: string;
+      expirationMonth: number;
+      expirationYear: number;
+      cvv: string;
+   }
+   amount: number;
+   description: string;
+   store: string;
+}
+
+interface PaymentGrpcService {
+   payment(data: PaymentData): Observable<void>;
+}
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService implements OnModuleInit {
+   private paymentGrpcService: PaymentGrpcService;
+
+   constructor(@Inject('PAYMENT_PACKAGE') private clientGrpc: ClientGrpc) { }
+
+   onModuleInit() {
+      this.paymentGrpcService = this.clientGrpc.getService<PaymentGrpcService>('PaymentService');
+   }
+
+   async payment(data: PaymentData) {
+      return await this.paymentGrpcService.payment(data);
+   }
+}
