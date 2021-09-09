1815db8f89836b6576a1ba42c7276f2d36dceebe
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-09-09 11:11:15 -0300

    orders.service:implementando payment no create e transaction; implementando funcao findAll

--- store-api/src/orders/orders.service.ts
+++ store-api/src/orders/orders.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from 'src/products/entities/product.entity';
-import { In, Repository } from 'typeorm';
+import { Connection, In, Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { Order } from './entities/order.entity';
+import { Order, OrderStatus } from './entities/order.entity';
+import { PaymentService } from './payment/payment.service';
 
 @Injectable()
 export class OrdersService {
    constructor(
       @InjectRepository(Order) private orderRepo: Repository<Order>,
-      @InjectRepository(Product) private productRepo: Repository<Product>
+      @InjectRepository(Product) private productRepo: Repository<Product>,
+      private paymentService: PaymentService,
+      private connection: Connection
    ) { }
 
    async create(createOrderDto: CreateOrderDto) {
@@ -27,12 +30,47 @@ export class OrdersService {
          )
          item.price = product.price
       })
-      
-      return this.orderRepo.save(order)
+
+      const queryRunner = this.connection.createQueryRunner()
+      await queryRunner.connect()
+      await queryRunner.startTransaction()
+
+      try {
+         const newOrder = await queryRunner.manager.save(order)
+
+         await this.paymentService.payment({
+            creditCard: {
+               name: order.credit_card.name,
+               number: order.credit_card.number,
+               expirationMonth: order.credit_card.expiration_month,
+               expirationYear: order.credit_card.expiration_year,
+               cvv: order.credit_card.cvv,
+            },
+            amount: order.total,
+            store: process.env.STORE_NAME,
+            description: `Produtos: ${products.map(p => p.name).join(', ')}`
+         })
+
+         await queryRunner.manager.update(
+            Order,
+            { id: newOrder.id },
+            { status: OrderStatus.Approved }
+         )
+
+         queryRunner.commitTransaction()
+         return newOrder
+      } catch (error) {
+         await queryRunner.rollbackTransaction()
+         throw error
+      } finally {
+         await queryRunner.release()
+      }
    }
 
-   findAll() {
-      return `This action returns all orders`;
+   async findAll() {
+      const orders = await this.orderRepo.find();
+      console.log(orders.length);
+      return orders
    }
 
    findOne(id: number) {
